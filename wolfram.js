fa46783ca449ac54d6e2191b86b15618d64a6291
Amit Patel <Amit Patel>
2016-05-22 17:16:51 -0400

    fixed bug

--- wolfram.js
+++ wolfram.js
@@ -47,7 +47,6 @@ Wolfram.prototype.query = function(input, assumptions, context, cb){
 			var pods = result['queryresult']['pod'];
 			var img = findpod('Result', 'img', pods)[0]['$']['src'];
 			var ret = {'text': findpod('Result', 'plaintext', pods)[0], 'image': img};
-			console.log(ret);
 			context.image = ret.image;
 			context.text = ret.text; 
 			cb(context);
